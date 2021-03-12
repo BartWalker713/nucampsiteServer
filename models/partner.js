b9f5503eda2e303b899dcbfe73fee0a870f2e5fb
Bart Walker <srvwish22@gmail.com>
2021-03-12 11:30:45 -0600

    week 2 workshop

--- models/partner.js
+++ models/partner.js
@@ -1,9 +1,5 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-
-require('mongoose-currency').loadType(mongoose);
-const Currency = mongoose.Types.Currency;
-
 const partnerSchema = new Schema({
     name: {
         type: String,
@@ -11,25 +7,20 @@ const partnerSchema = new Schema({
         unique: true
     },
     image: {
-      type: String,
-      required: true
-  },
-  featured: {
-      type: Boolean,
-      default: false
-  },
+        type: String,
+        required: true
+    },
+    featured: {
+        type: Boolean,
+        default: false
+    },
     description: {
         type: String,
         required: true
     },
-    comments: [commentSchema]
-}, 
-{
-    timestamps: true
-});
-
-
-
+},
+    {
+        timestamps: true
+    });
 const Partner = mongoose.model('Partner', partnerSchema);
-
 module.exports = Partner;
